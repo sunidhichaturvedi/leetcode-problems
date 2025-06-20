684d09b12ae7747710f43a9602bbe516c8e75040
Sunidhi Chaturvedi <sunidhichaturvedi258012@gmail.com>
2025-06-20 16:22:42 +0530

    Create REVERSE OF A STRING.cpp

---
+++ REVERSE OF A STRING.cpp
@@ -0,0 +1,15 @@
+//Reverse of a string 
+
+//CODE-
+
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+     int st=0,end=s.size()-1;
+        while (st<end){
+            swap(s[st++],s[end--]);
+        }
+        
+    }
+};
+
